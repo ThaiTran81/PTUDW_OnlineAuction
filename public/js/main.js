71eb912a78f460a146dec2a09e38aaf72e2e1d16
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2021-12-05 10:58:36 +0700

    create navbar

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("navbar").style.backgroundColor = "#560BAD";
+    document.getElementById("seperate").style.display="none";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "unset";
+    document.getElementById("seperate").style.display="block";
+  }
+}
