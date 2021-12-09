a416d4a9193638136c5d30c1a0bce7e4faf60b68
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2021-12-09 11:08:01 +0700

    register page

---
+++ public/js/register.js
@@ -0,0 +1,8 @@
+function next(){
+    document.getElementById('login').style.display = "block";
+    document.getElementById('signup').style.display = "none";
+}
+function prev(){
+    document.getElementById('login').style.display = "none";
+    document.getElementById('signup').style.display = "block";
+}
