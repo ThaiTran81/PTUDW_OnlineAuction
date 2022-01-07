21a65dd0f99e6fc121f640550c196f0903388a16
Trần Hoàng Thái <63252276+ThaiTran81@users.noreply.github.com>
2022-01-08 01:53:20 +0700

    add view detail

--- models/product.model.js
+++ models/product.model.js
@@ -24,7 +24,7 @@ export default {
         return knex('product')
             .join('type', 'product.typID', '=', 'type.typID')
             .select('product.proID', 'product.typID', 'type.catID', 'product.proName', 'product.startPrice', 'product.buyNow', 'product.startDate', 'product.endDate')
-            .where({'type.catID':catId, 'type.typID':typeId}).limit(limit).offset(offset);
+            .where({'type.catID': catId, 'type.typID': typeId}).limit(limit).offset(offset);
     },
     async countByCatId(catId) {
         const list = await knex('product')
@@ -33,17 +33,22 @@ export default {
         return list[0].amount;
     },
 
-    findTopEnd(n){
-        return knex('product').limit(n).where('endDate','>',new Date()).orderBy('endDate');
+    findTopEnd(n) {
+        return knex('product').limit(n).where('endDate', '>', new Date()).orderBy('endDate');
     },
 
-    findTopBid(n){
+    findTopBid(n) {
         return knex.raw('SELECT p.proID, p.proName, p.endDate, p.buyNow, COUNT(h.UID) AS bidNum\n' +
             'FROM product p JOIN historyauc h on p.proID=h.proID\n' +
             'WHERE p.endDate >= NOW()\n' +
             'GROUP BY p.proID\n' +
             'ORDER BY COUNT(h.UID) DESC\n' +
-            'LIMIT '+n);
+            'LIMIT ' + n);
+    },
+
+    findProductPrice(id) {
+        return knex('history')
     }
+
 
 }
