24d1f1a7601ebcdfd511041c78e0f958fd5c9262
Viru <minhnhatnan@gmail.com>
2022-01-12 21:15:04 +0700

    Modify seller dashboard

--- models/seller.model.js
+++ models/seller.model.js
@@ -13,12 +13,22 @@ export default {
     },
     async findAllHasWinnerProduct(UID){
         const winPros = await knex.raw('SELECT p.*,u.UID,u.`name`,u.email,h.price FROM product p JOIN historyauc h ON p.proID=h.proID JOIN (\n' +
-            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.ownerUID = ' + UID + ' ORDER BY p.endDate ASC')
+            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.buyUID = -1 AND p.ownerUID = ' + UID + ' ORDER BY p.endDate ASC')
         return winPros[0];
     },
     async findHasWinnerProductByPage(UID, limit, offset){
         const winPros = await knex.raw('SELECT p.*,u.UID,u.`name`,u.email,h.price, 0 AS isRate FROM product p JOIN historyauc h ON p.proID=h.proID JOIN (\n' +
-            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.ownerUID = ' + UID + ' ORDER BY p.isRated ASC LIMIT ' + limit + ' OFFSET ' + offset);
+            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.buyUID = -1 AND p.ownerUID = ' + UID + ' ORDER BY p.isRated ASC LIMIT ' + limit + ' OFFSET ' + offset);
+        return winPros[0];
+    },
+    async findAllHasBuyerProduct(UID){
+        const winPros = await knex.raw('SELECT p.*,u.UID,u.`name`,u.email,h.price FROM product p JOIN historyauc h ON p.proID=h.proID JOIN (\n' +
+            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.buyUID <> -1 AND p.ownerUID = ' + UID + ' ORDER BY p.endDate ASC')
+        return winPros[0];
+    },
+    async findHasBuyerProductByPage(UID, limit, offset){
+        const winPros = await knex.raw('SELECT p.*,u.UID,u.`name`,u.email,h.price, 0 AS isRate FROM product p JOIN historyauc h ON p.proID=h.proID JOIN (\n' +
+            'SELECT h2.proID,MAX(h2.price) AS price FROM historyauc h2 JOIN currentauction c ON h2.UID=c.UID WHERE c.isBlock !=1 GROUP BY h2.proID) t1 ON t1.proID=h.proID AND h.price=t1.price JOIN users u ON h.UID=u.UID WHERE p.proID IN (SELECT proID FROM winauction) AND p.buyUID <> -1 AND p.ownerUID = ' + UID + ' ORDER BY p.isRated ASC LIMIT ' + limit + ' OFFSET ' + offset);
         return winPros[0];
     }
 }
